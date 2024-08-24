bb794dd81f06ce4936a35c6cfedbf86a64ca1c31
HinnMachache <bennyhinn253.bh@gmail.com>
2024-08-25 01:43:23 +0300

    Login Form, data Validaton

--- flaskIntroduction/app.py
+++ flaskIntroduction/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 # import json
 
@@ -40,11 +40,20 @@ def makk():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template("register.html", form=form, title='Register')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccesful. Please check username and password', 'danger')
     return render_template("login.html", form=form, title='Login')
 
 
