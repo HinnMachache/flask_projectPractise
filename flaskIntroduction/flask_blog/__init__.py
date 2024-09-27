1597bb3a5f731502f74aba8e2380a80f412aedd2
HinnMachache <bennyhinn253.bh@gmail.com>
2024-09-27 23:35:25 +0300

    Machine Learning Methods

--- flaskIntroduction/flask_blog/__init__.py
+++ flaskIntroduction/flask_blog/__init__.py
@@ -7,28 +7,35 @@ from flask_blog.config import Config
 # import json
 
 
-app = Flask(__name__)
-
-db = SQLAlchemy(app)
-brcypt = Bcrypt(app)
-app.config.from_object(Config)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+brcypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 
-from flask_blog.main.routes import main
-from flask_blog.users.routes import users
-from flask_blog.posts.routes import posts   # Import the Blueprint
+def create_app(config_class=Config):
+    app = Flask(__name__)
 
-app.register_blueprint(main)
-app.register_blueprint(users)
-app.register_blueprint(posts) # Register the blueprints
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    brcypt.init_app(app)
+    login_manager.init_app(app)
+    
+
+    from flask_blog.main.routes import main
+    from flask_blog.users.routes import users
+    from flask_blog.posts.routes import posts   # Import the Blueprint
+
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts) # Register the blueprints
 
 
-# 1. Import Blueprint
-# 2. Restructure modules into specific related packages
-# 3. Restructure import statements..
-# 4. change url_for() functions to include blueprint module file_names
-# 5.  Change login
-# 6. Test the changes..
+    # 1. Import Blueprint
+    # 2. Restructure modules into specific related packages
+    # 3. Restructure import statements..
+    # 4. change url_for() functions to include blueprint module file_names
+    # 5.  Change login
+    # 6. Test the changes..
